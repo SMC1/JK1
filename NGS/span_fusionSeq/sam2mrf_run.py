16b2f78946bdb1115ea1ffb4a0e8f2f999b2c036
Jinkuk Kim <kimjinkuk@gmail.com>
2012-09-11 13:12:46 +0900

    regular update

--- NGS/span_fusionSeq/sam2mrf_run.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import os
-
-dataDir = '/data2/FusionSeq'
-
-i = '03'
-
-os.system('/home/tools/RSEQtools-0.6/sam2mrf < %s/RNASeq_SMC1_S%s_result.sam > %s/RNASeq_SMC1_S%s_result.mrf' % (dataDir,i,dataDir,i))
