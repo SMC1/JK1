cd0ce2b88fd530dc6896b2b3eb3a15c02d2c7884
Jinkuk Kim <kimjinkuk@gmail.com>
2012-11-20 15:38:32 +0900

    asynchronous batch execution for samples: - with alignment file - without report file

--- NGS/splice_gsnap/skipping/exonSkip_batch_WG.py
+++ NGS/splice_gsnap/skipping/exonSkip_batch_WG.py
@@ -6,17 +6,20 @@ import mybasic
 
 def exonSkip_batch(inDirName,outDirName):
 
-	sampNameL = list(set([re.match('.*/(.*).qlog:Processed.*',line).group(1) for line in os.popen('grep Processed %s/*.qlog' % inDirName)]))
-	sampNameL.sort()
+	sampNameS = set([re.match('.*/(.*).qlog:Processed.*',line).group(1) for line in os.popen('grep -H Processed %s/*.qlog' % inDirName)])
+	excSampNameS = set([re.search('([^/ ]+)_splice_exonSkip_report.txt',line).group(1) for line in os.popen('ls -l %s/*_exonSkip_report.txt' % inDirName)])
 
+	sampNameL = list(sampNameS.difference(excSampNameS))
+	sampNameL.sort()
+	
 	print 'Samples: %s (%s)' % (sampNameL, len(sampNameL))
 
 	for sampN in sampNameL:
 
-		if sampN not in ['G17197.TCGA-06-0211-01B-01R-1849-01.2']:
-			continue
+#		if sampN not in ['G17197.TCGA-06-0211-01B-01R-1849-01.2']:
+#			continue
 
-		outLogFile = open('%s/%s.proc.log' % (outDirName,sampN),'w')
+		outLogFile = open('%s/%s_splice_exonSkip.log' % (outDirName,sampN),'w')
 
 		outLogFile.write('[%s]\n' % sampN)
 
