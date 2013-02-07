c3102bff2bd55b82673265f6e2323ee5404192e1
Jinkuk Kim <kimjinkuk@gmail.com>
2013-02-07 19:38:17 +0900

    update

--- Integration/prepDB_array_gene_expr.py
+++ Integration/prepDB_array_gene_expr.py
@@ -4,9 +4,23 @@ import sys, getopt
 import mybasic
 
 
-def function(inGctFileName):
+def function(inGctFileName,geneList=None):
 
-	inFile = open(inGctFile)
+	inFile = open(inGctFileName)
+
+	inFile.readline()
+	inFile.readline()
+
+	sampleIdL = inFile.readline()[:-1].split('\t')
+
+	for line in inFile:
+
+		dataL = line[:-1].split('\t')
+
+		if geneList and dataL[0] in geneList:
+			
+			for i in range(2,len(dataL)):
+				sys.stdout.write('S%s\t%s\t%s\n' % (sampleIdL[i],dataL[0],dataL[i]))
 
 
 optL, argL = getopt.getopt(sys.argv[1:],'i:o:t',[])
@@ -17,4 +31,4 @@ if '-i' in optH and '-o' in optH:
 
 	function(optH['-i'], optH['-o'])
 
-function('/EQL1/NSL/array_gene/NSL_GBM_93_zNorm.gct')
+function('/EQL1/NSL/array_gene/NSL_GBM_93_zNorm.gct',['EGFR'])
--- NGS/fastq/link.py
+++ NGS/fastq/link.py
@@ -12,7 +12,7 @@ def link(dirName,outDirName,filePattern):
 		ro = re.match(filePattern, fileN)
 
 		if ro:
-			os.system('ln -s %s/%s %s/%s.%s.fq.gz' % (dirName,fileN, outDirName,ro.group(1),ro.group(2)))
+			os.system('ln -s %s/%s %s/%s.%s.fq' % (dirName,fileN, outDirName,ro.group(1),ro.group(2)))
 
 
 #link('/EQL1/NSL/WXS/fastq', '(.*)_[ATGC]{6}_L005_R([12])_001\.fastq\.gz')
@@ -20,5 +20,6 @@ def link(dirName,outDirName,filePattern):
 #link('/EQL1/NSL/WXS_trueSeq/fastq', '/EQL1/NSL/WXS_trueSeq/fastq/link', '(.*)_R([12])\.fq\.gz')
 #link('/EQL1/NSL/RNASeq/fastq', '/EQL1/NSL/RNASeq/fastq/link', '(.*)_[ATGC]{6}_L008_R([12])_001\.fastq\.gz')
 #link('/EQL1/NSL/RNASeq/fastq/screening', '/EQL1/NSL/RNASeq/fastq/link', '(.*)_[ATGC]{6}_L[0-9]{3}_R([12])_001\.fastq\.gz')
+#link('/EQL1/NSL/RNASeq/fastq/link2', '/EQL1/NSL/RNASeq/fastq/link2/renamed', '(.*)_[ATGC]{6}_L[0-9]{3}_R([12])_001\.fastq\.gz')
 
-link('/EQL1/NSL/RNASeq/fastq/link2', '/EQL1/NSL/RNASeq/fastq/link2/renamed', '(.*)_[ATGC]{6}_L[0-9]{3}_R([12])_001\.fastq\.gz')
+link('/EQL1/NSL/RNASeq/fastq/30nt', '/EQL1/NSL/RNASeq/fastq/30nt/old', '(.*)_[ATGC]{6}_L[0-9]{3}_30nt\.([12])\.fastq')
