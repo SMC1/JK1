71ba79e22c577ed9baa78cfffb03ebc4587eb1f4
Jinkuk Kim <kimjinkuk@gmail.com>
2013-02-22 14:16:05 +0900

    update

--- Integration/splice_eiJunc.sql
+++ Integration/splice_eiJunc.sql
@@ -10,4 +10,4 @@ CREATE TABLE splice_eiJunc (
 	primary key (samp_id,loc)
 );
 
-LOAD DATA LOCAL INFILE "/EQL1/NSL/RNASeq/alignment/splice_Z/ei_junc/splice_eiJunc_NSL_RTK1.dat" INTO TABLE splice_eiJunc;
+LOAD DATA LOCAL INFILE "/EQL1/NSL/RNASeq/alignment/splice_eiJunc_NSL_RTK1.dat" INTO TABLE splice_eiJunc;
