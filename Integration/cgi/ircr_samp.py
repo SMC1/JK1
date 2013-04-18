10e26de3aab5ae85c11bf183705d81e8ae4b72c9
Jinkuk Kim <kimjinkuk@gmail.com>
2013-04-18 15:05:32 +0900

    update

--- Integration/cgi/ircr_samp.py
+++ Integration/cgi/ircr_samp.py
@@ -14,53 +14,76 @@ def main():
 
 	## sample information
 
-	print '<p><b>%s (%s)</b></p> <p><ul>' % (sId,mycgi.db2dsetN[dbN])
+	if mode=='samp':
 
-	cursor.execute('select tag from sample_tag where samp_id="%s"' % (sId))
-	tags = [x[0] for x in cursor.fetchall()]
+		print '<p><b>%s (%s)</b></p> <p><ul>' % (sId,mycgi.db2dsetN[dbN])
 
-	cursor.execute('select 1 from splice_normal where samp_id="%s" limit 1' % (sId))
-	avail_RSq = cursor.fetchone()
+		cursor.execute('select tag from sample_tag where samp_id="%s"' % (sId))
+		tags = [x[0] for x in cursor.fetchall()]
 
-	# panel_S
-	print '<li>Panel: %s' % ', '.join(map(lambda x: x[6:], filter(lambda x: x.startswith('panel_'), tags)))
+		cursor.execute('select 1 from splice_normal where samp_id="%s" limit 1' % (sId))
+		avail_RSq = cursor.fetchone()
 
-	# tum,inv
-	tL = filter(lambda x: x.startswith('tum_') or x.startswith('inv_'), tags)
-	tL.sort(lambda x,y: cmp(y,x))
-	print '<li>Phenotype: %s' % ', '.join(tL)
+		# panel_S
+		print '<li>Panel: %s' % ', '.join(map(lambda x: x[6:], filter(lambda x: x.startswith('panel_'), tags)))
 
-	# data availability
-	tL = filter(lambda x: x.startswith('XSeq_'), tags)
-	if avail_RSq:
-		tL.append('RNA-Seq') 
-	print '<li>Available: %s' % ', '.join(tL)
-		
-	# matched
-	text = ','.join(map(lambda x: x[5:], filter(lambda x: x.startswith('pair_'), tags)))
-	print '<li>Matched: %s' % linkSamp(text).replace(',',', ')
+		# tum,inv
+		tL = filter(lambda x: x.startswith('tum_') or x.startswith('inv_'), tags)
+		tL.sort(lambda x,y: cmp(y,x))
+		print '<li>Phenotype: %s' % ', '.join(tL)
 
-	print '</ul></p> <font size=2>'
+		# data availability
+		tL = filter(lambda x: x.startswith('XSeq_'), tags)
+		if avail_RSq:
+			tL.append('RNA-Seq') 
+		print '<li>Available: %s' % ', '.join(tL)
+			
+		# matched
+		text = ','.join(map(lambda x: x[5:], filter(lambda x: x.startswith('pair_'), tags)))
+		print '<li>Matched: %s' % linkSamp(text).replace(',',', ')
+
+		print '</ul></p>'
+
 	#census gene
 	cursor.execute("select distinct gene_sym from common.census")
 	census_gene = [x for (x,) in cursor.fetchall()]
 
 	## variant information
 
-	for spec in specL:
+	if mode=='type':
+		tableL = [specL[dTypeH[dType][0]]]
+	else:
+		tableL = specL
 
-		(dType,colL,tblN,cond,ordr) = spec
+	print '<font size=2>'
 
-		cursor.execute("select %s from %s where samp_id = '%s' and %s order by %s" % (','.join(colL), tblN, sId, cond, ordr))
+	for spec in tableL:
+
+		(dt,colL,tblN,cond,ordr) = spec
+
+		if mode=='type':
+			colL = ["samp_id"] + colL
+
+		if mode=='samp':
+			cursor.execute("select %s from %s where samp_id = '%s' and %s order by %s" % (','.join(colL), tblN, sId, cond, ordr))
+		else:
+			cursor.execute("select %s from %s where %s order by %s" % (','.join(colL), tblN, dTypeH[dt][1], ordr))
+
 		data = cursor.fetchall()
 
 		# theader
-		print '<br><a name="%s_"></a><b>%s</b> (%s, %s):' % (dType,dType,len(data),('All' if cond=='True' else cond))
+		if mode=='samp':
+			if dt in ['Fusion','ExonSkipping','3pDeletion']:
+				print '<br id="%s_"><b><a href="ircr_samp.py?dbN=%s&dType=%s">%s</a></b> (%s, %s, %s):' % (dt,dbN,dt,dt,len(data),('All' if cond=='True' else cond),ordr)
+			else:
+				print '<br id="%s_"><b>%s</b> (%s, %s, %s):' % (dt,dt,len(data),('All' if cond=='True' else cond),ordr)
+		else:
+			print '<font size=3><p id="%s_"><b>%s</b> (%s, %s, %s):</font></p>' % (dt,dt,len(data),dTypeH[dt][1],ordr)
 
 		print '''
 			<a href="#%s_" onclick="$('#%s tbody tr').hide()">None</a> | <a href="#%s_" onclick='filter("%s","census")'>Census</a> | <a href="#%s_" onclick="$('#%s tbody tr').show()">All</a><br>
 			<table border="1" cellpadding="0" cellspacing="0" id="%s">
-			<thead>''' % (dType,dType,dType,dType,dType,dType,dType)
+			<thead>''' % ((dt,)*7)
 
 		print '<tr>'
 		for colN in colL:
@@ -83,8 +106,10 @@ def main():
 						print '<td><a href="ircr.py?dbN=%s&geneN=%s" class="census"> %s </a></td>' % (dbN,row[j],content)
 					else:
 						print '<td><a href="ircr.py?dbN=%s&geneN=%s" class="not_census"> %s </a></td>' % (dbN, row[j],content)
-				elif colN=='ch_type':
+				elif colN == 'ch_type':
 					print '<td nowrap> %s </td>' % content
+				elif colN == 'samp_id':
+					print '<td nowrap> <a href="ircr_samp.py?dbN=%s&sId=%s"> %s </a> </td>' % (dbN,content,content)
 				elif 'coord' in colN:
 					print '<td nowrap><a href="http://genome.ucsc.edu/cgi-bin/hgTracks?db=hg19&position=%s"> %s </a></td>' % (content[1:],content)
 				else:
@@ -115,34 +140,32 @@ else :
 	mode = 'samp'
 	sId = "TCGA-06-5411"
 
+dTypeH = {
+	'Fusion': (1,'nPos>10'),
+	'ExonSkipping': (2,'nPos>20'),
+	'3pDeletion': (3,'nReads_w and nReads/nReads_w>=50')
+	}
+
 specL = [
 	('Mutation', ["concat(strand,chrom,':',chrSta,'-',chrEnd) coord_hg19", "ref", "alt", "nReads_ref", "nReads_alt", \
 		"gene_sym", "ch_dna", "ch_aa", "ch_type", "cosmic", "mutsig", "if(census is NULL,'',census) census"], 't_mut', 'True', 'gene_sym,chrSta'),
-	('Fusion', ["loc1 coord1", "loc2 coord2", "gene_sym1", "gene_sym2", "ftype", "exon1", "exon2", "frame", "nPos"], 'splice_fusion', 'nPos>2', 'nPos desc'),
-	('ExonSkipping', ["loc1 coord1", "loc2 coord2", "gene_sym", "frame", "delExons", "exon1", "exon2", "nReads", "nPos"], 'splice_skip', 'nPos>2', 'nPos desc'),
-	('3pDeletion', ["loc coord_hg19", "gene_sym", "juncInfo", "juncAlias", "nReads","nReads_w"], 't_3p_af', 'nReads_w and nReads>5', 'nReads/nReads_w desc'),
+	('Fusion', ["loc1 coord1", "loc2 coord2", "gene_sym1", "gene_sym2", "ftype", "exon1", "exon2", "frame", "nPos","nReads","nReads_w1","nReads_w2"], 'splice_fusion_AF', 'nPos>2', 'nPos desc'),
+	('ExonSkipping', ["loc1 coord1", "loc2 coord2", "gene_sym", "frame", "delExons", "exon1", "exon2", "nPos", "nReads","nReads_w1","nReads_w2"], 'splice_skip_AF', 'nPos>2', 'nPos desc'),
+	('3pDeletion', ["loc coord_hg19", "gene_sym", "juncInfo", "juncAlias", "nReads","nReads_w"], 'splice_eiJunc_AF', 'nReads_w and nReads>5', '(nReads/nReads_w) desc'),
 	('ExprOutlier',["gene_sym","expr_MAD","q25","median","q75"],'t_outlier', '(expr_MAD >= q75 + 3*(q75-q25) or expr_MAD <= q25 - 3*(q75-q25))', 'gene_sym')
 	]
 
 (con,cursor) = mycgi.connectDB(db=dbN)
 
-cursor.execute('create temporary table t_mut as \
-	select mutation.*,concat(tumor_soma,";",tumor_germ,";",mut_type,";",tloc_partner) census \
-	from mutation left join census using (gene_sym) where samp_id="%s"' % sId)
+if mode == 'samp':
 
-cursor.execute('create temporary table t_3p as select * from splice_eiJunc where samp_id="%s"' % sId)
-cursor.execute('alter table t_3p add index (loc)')
+	cursor.execute('create temporary table t_mut as \
+		select mutation.*,concat(tumor_soma,";",tumor_germ,";",mut_type,";",tloc_partner) census \
+		from mutation left join census using (gene_sym) where samp_id="%s"' % sId)
 
-cursor.execute('create temporary table t_splice_normal as select loc1,sum(nReads) nReads_w from splice_normal where samp_id="%s" group by loc1' % sId)
-cursor.execute('alter table t_splice_normal add index (loc1)')
-
-cursor.execute('create temporary table t_3p_af as \
-	select samp_id,loc,gene_sym,juncInfo,juncAlias,t1.nReads,t2.nReads_w \
-	from t_3p t1 left join t_splice_normal t2 on t1.loc=t2.loc1 where t1.samp_id="%s"' % sId)
-
-cursor.execute('create temporary table t_outlier as \
-	select samp_id,mad.gene_sym, expr_MAD, q25, median, q75 from array_gene_expr_MAD mad \
-	join gene_expr_stat stat using (gene_sym) where samp_id="%s"' % sId)
+	cursor.execute('create temporary table t_outlier as \
+		select samp_id,mad.gene_sym, expr_MAD, q25, median, q75 from array_gene_expr_MAD mad \
+		join gene_expr_stat stat using (gene_sym) where samp_id="%s"' % sId)
 
 print "Content-type: text/html\r\n\r\n";
 
@@ -150,8 +173,12 @@ print '''
 <!DOCTYPE HTML>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-<title>%s (%s)</title>''' % (sId,mycgi.db2dsetN[dbN])
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">'''
+
+if mode =='samp':
+	print '<title>%s (%s)</title>' % (sId,mycgi.db2dsetN[dbN])
+else:
+	print '<title>%s (%s)</title>' % (dType,mycgi.db2dsetN[dbN])
 
 print '''
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.5.2.js"></script>
