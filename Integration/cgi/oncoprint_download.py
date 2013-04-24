36baac7e69e81f4c30481330b7ee7dd17fa51561
yenakim <cassie.ynk@gmail.com>
2013-04-24 15:10:11 +0900

    add oncoprint_download.py

---
+++ Integration/cgi/oncoprint_download.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import cgi, sys
+
+form = cgi.FieldStorage()
+xml_value = form.getvalue("xml")
+svg_file = open("oncoprint_converter.svg", "w")
+svg_file.write(xml_value)
+svg_file.close()
+
+print 'Content-Disposition: attachment; filename="oncoprint.svg"\r\n\r\n';
+
+download_file = open("oncoprint_converter.svg", "r")
+
+for s in download_file.readlines():
+	print s
+
+download_file.flush()
+download_file.close()
