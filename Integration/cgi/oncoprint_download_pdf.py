c0f15a95f75732e8092c4816e7859d77c8a47adf
yenakim <cassie.ynk@gmail.com>
2013-05-20 16:33:49 +0900

    update oncoprint_patient.py / oncoprint_download_pdf.py

---
+++ Integration/cgi/oncoprint_download_pdf.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import cgi, sys
+from svglib.svglib import svg2rlg
+from reportlab.graphics import renderPDF
+
+form = cgi.FieldStorage()
+xml_value = form.getvalue("xml2")
+svg_file = open("oncoprint_converter.svg", "w")
+svg_file.write(xml_value)
+svg_file.close()
+
+print 'Content-Disposition: attachment; filename="oncoprint.pdf"\r\n\r\n';
+#print 'Content-Type: application/pdf \r\n\r\n';
+
+drawing = svg2rlg("oncoprint_converter.svg")
+renderPDF.drawToFile(drawing, "oncoprint_converter.pdf")
+
+download_file = open("oncoprint_converter.pdf", "r")
+
+for s in download_file.readlines():
+	print s
+
+download_file.flush()
+download_file.close()
