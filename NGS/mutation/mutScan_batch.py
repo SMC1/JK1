00822d1a061894c11eaeaf4d184c8562a335e4cc
Jinkuk Kim <kimjinkuk@gmail.com>
2013-05-23 15:44:29 +0900

    update

---
+++ NGS/mutation/mutScan_batch.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import sys, os, re, getopt
+import mybasic, mygenome
+
+minCover = 3
+minFreq = 0.01
+
+def main(inputDirN, outputDirN, pbs=False):
+
+	inputFileNL = os.listdir(inputDirN)
+	inputFileNL = filter(lambda x: re.match('.*chr.*\.pileup_proc', x),inputFileNL)
+
+	sampNL = list(set([re.match('(.*)_chr.*\.pileup_proc',inputFileN).group(1) for inputFileN in inputFileNL]))
+	sampNL.sort()
+
+	print 'Samples: %s' % sampNL
+
+	for sampN in sampNL:
+
+#		if sampN not in ['NS09_671T']:
+#			continue
+
+		if pbs:
+
+			print sampN
+
+			os.system('echo "~/JK1/NGS/mutation/mutScan.py -s %s -i %s -o %s/%s.mutscan -c %s -f %s" | \
+				qsub -N %s -o %s/%s.mutscan.log -j oe' % \
+				(sampN, inputDirN, outputDirN,sampN,minCover,minFreq, sampN, outputDirN,sampN))
+		else:
+
+			print sampN
+
+			os.system('(~/JK1/NGS/mutation/mutScan.py -s %s -i %s -o %s/%s.mutscan -c %s -f %s) 2> %s/%s.mutscan.log' % \
+				(sampN, inputDirN, outputDirN,sampN,minCover,minFreq, outputDirN,sampN))
+
+
+optL, argL = getopt.getopt(sys.argv[1:],'i:o:p:',[])
+
+optH = mybasic.parseParam(optL)
+
+main('/EQL1/NSL/exome_bam/mutation/link/proc', '/EQL1/NSL/exome_bam/mutation/mutscan', True)
---
+++ NGS/mutation/procPileup_split.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+
+import sys, os, re, getopt
+import mybasic
+
+
+patt = re.compile('(\$)|(\^.)')
+patt_indel = re.compile('([\+\-]{1})([0-9]+)')
+
+def main(inFilePath,outFileDir,qualCutoff=15):
+
+	inFileTitle = inFilePath.split('/')[-1].split('.pileup')[0]
+
+	inFile = open('%s' % inFilePath)
+	outFile = 0
+
+	curChrom = ''
+
+	for line in inFile:
+
+		tL = line[:-1].split('\t')
+
+		if tL[0] != curChrom:
+			if outFile:
+				outFile.close()
+			outFile = open('%s/%s_%s.pileup_proc' % (outFileDir,inFileTitle,tL[0]),'w')
+			curChrom = tL[0]
+
+		baseStr = tL[-2]
+		qualStr = tL[-1]
+		
+		indelL = patt_indel.findall(baseStr)
+		
+		for (sign,num) in indelL:
+			baseStr = re.sub('\%s%s[ACGTNacgtn]{%s}' % (sign,num,num),'',baseStr)
+
+		baseStr  = patt.sub('',baseStr)
+
+		if len(baseStr) != len(qualStr):
+			print 'Error:', baseStr, qualStr
+			raise Exception
+
+		baseL = []
+
+		for i in range(len(baseStr)):
+			if ord(qualStr[i])-33 >= 15:
+				baseL.append(baseStr[i])
+
+		baseStr = ''.join(baseL)
+		total = len(baseStr)
+
+		ref = baseStr.count('.') + baseStr.count(',')
+
+		baseStr = baseStr.replace('.','').replace(',','').upper()
+
+		outFile.write('%s:%s,%s,%s,%s,%s\n' % (tL[0],tL[1],total,tL[2].upper(),ref,baseStr))
+	
+	outFile.close()
+
+optL, argL = getopt.getopt(sys.argv[1:],'i:o:q:',[])
+
+optH = mybasic.parseParam(optL)
+
+if '-i' in optH and '-o' in optH and '-q' in optH:
+
+	main(optH['-i'], optH['-o'], optH['-q'] )
