28106a60093ed1db647678229f5f6581c3dbf974
Jinkuk Kim <kimjinkuk@gmail.com>
2013-05-26 17:35:17 +0900

    update

--- NGS/mutation/mutScan_batch.py
+++ NGS/mutation/mutScan_batch.py
@@ -4,6 +4,7 @@ import sys, os, re, getopt
 import mybasic, mygenome
 
 minCover = 3
+minMutReads = 2
 minFreq = 0.01
 
 def main(inputDirN, outputDirN, pbs=False):
@@ -25,19 +26,19 @@ def main(inputDirN, outputDirN, pbs=False):
 
 			print sampN
 
-			os.system('echo "~/JK1/NGS/mutation/mutScan.py -s %s -i %s -o %s/%s.mutscan -c %s -f %s" | \
+			os.system('echo "~/JK1/NGS/mutation/mutScan.py -s %s -i %s -o %s/%s.mutscan -c %s -m %s -f %s" | \
 				qsub -N %s -o %s/%s.mutscan.log -j oe' % \
-				(sampN, inputDirN, outputDirN,sampN,minCover,minFreq, sampN, outputDirN,sampN))
+				(sampN, inputDirN, outputDirN,sampN,minCover,minMutReads,minFreq, sampN, outputDirN,sampN))
 		else:
 
 			print sampN
 
-			os.system('(~/JK1/NGS/mutation/mutScan.py -s %s -i %s -o %s/%s.mutscan -c %s -f %s) 2> %s/%s.mutscan.log' % \
-				(sampN, inputDirN, outputDirN,sampN,minCover,minFreq, outputDirN,sampN))
+			os.system('(~/JK1/NGS/mutation/mutScan.py -s %s -i %s -o %s/%s.mutscan -c %s -m %s -f %s) 2> %s/%s.mutscan.log' % \
+				(sampN, inputDirN, outputDirN,sampN,minCover,minMutReads,minFreq, outputDirN,sampN))
 
 
 optL, argL = getopt.getopt(sys.argv[1:],'i:o:p:',[])
 
 optH = mybasic.parseParam(optL)
 
-main('/EQL1/NSL/exome_bam/mutation/link/proc', '/EQL1/NSL/exome_bam/mutation/mutscan', True)
+main('/EQL1/NSL/exome_bam/mutation/pileup_proc', '/EQL1/NSL/exome_bam/mutation/mutscan', True)
