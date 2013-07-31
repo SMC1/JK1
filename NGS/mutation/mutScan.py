a80f0697d1cbb2e759880ba6094a9324446c2082
Kernyu Park <kernyu@gmail.com>
2013-07-31 14:45:31 +0900

    update

--- NGS/mutation/mutScan.py
+++ NGS/mutation/mutScan.py
@@ -50,6 +50,8 @@ def main(sId, inDirName, outFileName, minCover=4, minMutReads=2, minFreq=0.01):
 				
 	outFile.close()
 
+	print 'Success'
+
 optL, argL = getopt.getopt(sys.argv[1:],'s:i:o:c:m:f:',[])
 
 optH = mybasic.parseParam(optL)
