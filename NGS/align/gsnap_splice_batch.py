3788dd25a9146e6003d1d558276482744f9056b7
Kernyu Park <kernyu@gmail.com>
2013-08-06 16:53:30 +0900

    update

--- NGS/align/gsnap_splice_batch.py
+++ NGS/align/gsnap_splice_batch.py
@@ -4,7 +4,7 @@ import sys, os, re, getopt
 import mybasic
 
 
-def align(inputDirN, outputDirN, nThreads, pbs=False):
+def align(inputDirN, outputDirN, nThreads, pbs=False, gz=True):
 
 	inputFileNL = os.listdir(inputDirN)
 	inputFileNL = filter(lambda x: re.match('.*\.fq\.gz', x),inputFileNL)
@@ -17,6 +17,13 @@ def align(inputDirN, outputDirN, nThreads, pbs=False):
 
 	print 'Samples: %s' % sampNL
 
+	if gz:
+		cmd = 'z'
+		ext = '.gz'
+	else:
+		cmd = ''
+		ext = ''
+
 	for sampN in sampNL:
 
 	#	if not sampN in ['G17678.TCGA-06-5417-01A-01R-1849-01.2']:
@@ -26,7 +33,7 @@ def align(inputDirN, outputDirN, nThreads, pbs=False):
 
 			print('%s' % sampN)
 
-			os.system('echo "zcat %s/%s.1.fq.gz %s/%s.2.fq.gz | \
+			os.system('echo "cmd"cat %s/%s.1.fq"ext" %s/%s.2.fq"ext" | \
 				/usr/local/bin/gsnap --db=hg19 --batch=5 --nthreads=%s --npath=1 -N 1 --use-splicing=refGene_knownGene_splicesites --nofails -Q \
 				 --query-unk-mismatch=1 > %s/%s_splice.gsnap" | qsub -N %s -o %s/%s.gsnap.qlog -j oe' % (inputDirN,sampN, inputDirN,sampN, nThreads, outputDirN,sampN, sampN, outputDirN,sampN))
 
@@ -34,7 +41,7 @@ def align(inputDirN, outputDirN, nThreads, pbs=False):
 
 			print('%s' % sampN)
 			
-			os.system('(zcat %s/%s.1.fq.gz %s/%s.2.fq.gz | \
+			os.system('("cmd"cat %s/%s.1.fq"ext" %s/%s.2.fq"ext" | \
 				/usr/local/bin/gsnap --db=hg19 --batch=5 --nthreads=%s --npath=1 -N 1 --use-splicing=refGene_knownGene_splicesites --nofails -Q \
 				--query-unk-mismatch=1 > %s/%s_splice.gsnap) 2> %s/%s.gsnap.qlog' % (inputDirN,sampN, inputDirN,sampN, nThreads, outputDirN,sampN, outputDirN,sampN))
 
