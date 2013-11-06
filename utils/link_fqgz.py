630eb84e3a4c65fea8995e32f5b4de00f3c3d0ad
ikarus97 <ikarus97@gmail.com>
2013-11-06 19:50:05 +0900

    New function: link_l() - takes list of regular expressions

--- utils/link_fqgz.py
+++ utils/link_fqgz.py
@@ -2,6 +2,19 @@
 
 import sys, os, re
 
+def link_l(dirName,outDirName,filePatternL,tag=''):
+	inputFilePL = os.popen('find %s -maxdepth 1 -name "*.fastq.gz"' % dirName, 'r')
+	for fileP in inputFilePL:
+		fileP = fileP[:-1]
+		fileN = fileP.split('/')[-1]
+		for filePattern in filePatternL:
+			ro = re.match(filePattern, fileN)
+			fileP = fileP.replace('(','\(').replace(')','\)').replace(' ','\ ')
+			print '[%s]' % fileP
+			if ro and not os.path.islink('%s/S%s.%s.fq.gz' % (outDirName,ro.group(1),ro.group(2).replace('(','\(').replace(')','\)'))):
+#				print 'ln -s %s %s/S%s.%s.fq.gz' % (fileP, outDirName,ro.group(1),ro.group(2).replace('(','\(').replace(')','\)'))
+				os.system('ln -s %s %s/S%s.%s.fq.gz' % (fileP, outDirName,ro.group(1),ro.group(2).replace('(','\(').replace(')','\)')))
+
 
 def link(dirName,outDirName,filePattern,tag=''):
 
@@ -32,4 +45,6 @@ def link(dirName,outDirName,filePattern,tag=''):
 #link('/EQL1/NSL/CGH/raw/Array_CGH/CGH_SCRI', '/data1/IRCR/CGH/fe/test', '(.*)\(([0-9]{3})\)\.txt')
 #link('/EQL1/NSL/CGH/raw/Array_CGH/CGH_SCRI', '/data1/IRCR/CGH/fe/test', '(.*)_([0-9]{3})\.txt')
 
-link('/EQL1/NSL/RNASeq/fastq', '/EQL1/NSL/RNASeq/fastq/link3', '.*(568|050|047|022|460)T.*R([12])_001\.fastq.gz')
+#link('/EQL1/NSL/RNASeq/fastq', '/EQL1/NSL/RNASeq/fastq/link3', '.*(568|050|047|022|460)T.*R([12])_001\.fastq.gz')
+##SGI 201310301 samples
+link_l('/EQL2/SGI_20131031/RNASeq/fastq','/EQL2/SGI_20131031/RNASeq/fastq/link',['([0-9]{1,2}[AB]).*R([12]).fastq.gz', 'GBM.*([0-9]{3})T.*R([12]).fastq.gz'])
