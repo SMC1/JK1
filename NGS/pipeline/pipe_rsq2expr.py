d4f5744964591a01e021cc4ae8970468031abd26
ikarus97 <ikarus97@gmail.com>
2013-11-12 15:48:40 +0900

    minor change to run RNAseq samples from SGI

--- NGS/pipeline/pipe_rsq2expr.py
+++ NGS/pipeline/pipe_rsq2expr.py
@@ -34,6 +34,7 @@ def main(inputFilePathL, projectN, clean=False, pbs=False):
 
 #		if sampN[1:4] not in ['096','145']:
 #			continue
+		print sampN
 
 		if pbs:
 			os.system('echo "python ~/JK1/NGS/pipeline/pipe_s_rsq2expr.py -i %s -n %s -p %s -c %s" | qsub -N %s -o %s/%s.Rsq_expr.qlog -j oe' % \
@@ -49,4 +50,4 @@ def main(inputFilePathL, projectN, clean=False, pbs=False):
 #main(glob('/EQL1/NSL/RNASeq/fastq/link/*.1.fq.gz'), projectN='RNAseq_fusion_FGFR', clean=False, pbs=True)
 #main(glob('/EQL6/NSL/WY/fastq/link/*.1.fq.gz'), projectN='WY_RNASeq_expr', clean=False, pbs=True)
 
-main(glob('/home/ihlee/test_data/*.1.fq.gz'), projectN='test_ini_rsq2expr2', clean=False, pbs=False)
+main(glob('/EQL2/SGI_20131031/RNASeq/fastq/link/*.1.fq.gz'), projectN='SGI20131031_rsq2expr', clean=False, pbs=True)
