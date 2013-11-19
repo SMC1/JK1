23370ae747e52e56223474f08f17e506b5f96590
ikarus97 <ikarus97@gmail.com>
2013-11-19 18:25:03 +0900

    don't check deletions in cosmic db preparation

--- Integration/prepDB_cosmic.py
+++ Integration/prepDB_cosmic.py
@@ -70,18 +70,18 @@ def main(inFileName,geneList=[]):
 			chr = 'M'
 			chrNum = 'M'
 
-		if vtype == 'del':
-			rm = re.search('([ACGT]+)', alt.upper())
-			## if deleted bases are specified
-			if alt != '' and rm:
-				## check if deleted bases are the same as reference sequences at the location
-				new_ref = os.popen('samtools faidx /data1/Sequence/ucsc_hg19/hg19.fa chr%s:%s-%s' % (chr,chrSta,chrEnd)).readlines()[1:]
-				new_ref = "".join(map(lambda x: x.rstrip().upper(), new_ref))
-				if new_ref == alt.upper():
-					chrSta = str(int(chrSta) - 1)
-					ref = os.popen('samtools faidx /data1/Sequence/ucsc_hg19/hg19.fa chr%s:%s-%s' % (chr,chrSta,chrEnd)).readlines()[1:]
-					ref = "".join(map(lambda x: x.rstrip().upper(), ref))
-					alt = ref[0]
+#		if vtype == 'del':
+#			rm = re.search('([ACGT]+)', alt.upper())
+#			## if deleted bases are specified
+#			if alt != '' and rm:
+#				## check if deleted bases are the same as reference sequences at the location
+#				new_ref = os.popen('samtools faidx /data1/Sequence/ucsc_hg19/hg19.fa chr%s:%s-%s' % (chr,chrSta,chrEnd)).readlines()[1:]
+#				new_ref = "".join(map(lambda x: x.rstrip().upper(), new_ref))
+#				if new_ref == alt.upper():
+#					chrSta = str(int(chrSta) - 1)
+#					ref = os.popen('samtools faidx /data1/Sequence/ucsc_hg19/hg19.fa chr%s:%s-%s' % (chr,chrSta,chrEnd)).readlines()[1:]
+#					ref = "".join(map(lambda x: x.rstrip().upper(), ref))
+#					alt = ref[0]
 
 		key = (chrNum,chrSta,chrEnd,strand,ref,alt)
 
