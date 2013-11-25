3918fb2e7e7c17db1378681ddd38c4567cb4b453
ikarus97 <ikarus97@gmail.com>
2013-11-25 12:42:41 +0900

    cmd change for new sample.

--- utils/link_fqgz.py
+++ utils/link_fqgz.py
@@ -13,8 +13,11 @@ def link_l(dirName,outDirName,filePatternL,tag='',RSQ=False, normalL=[]):
 			print '[%s]' % fileP
 			if ro:
 				## exceptional case (437,559) in sgi_20131031
-				if ro.group(1) in ['437','559']:
-					os.system('ln -s %s %s/S%s_X_RSq.%s.fq.gz' % (fileP, outDirName,ro.group(1),ro.group(2).replace('(','\(').replace(')','\)')))
+#				if ro.group(1) in ['437','559']:
+#					os.system('ln -s %s %s/S%s_X_RSq.%s.fq.gz' % (fileP, outDirName,ro.group(1),ro.group(2).replace('(','\(').replace(')','\)')))
+#					continue
+				## exceptional case (189T1,189T2) in sgi_20131119
+				if ro.group(1) in ['189']:
 					continue
 
 				if RSQ:
@@ -65,6 +68,9 @@ def link(dirName,outDirName,filePattern,tag='',RSQ=False, normalL=[]):
 #link('/EQL1/NSL/CGH/raw/Array_CGH/CGH_SCRI', '/data1/IRCR/CGH/fe/test', '(.*)_([0-9]{3})\.txt')
 
 #link('/EQL1/NSL/RNASeq/fastq', '/EQL1/NSL/RNASeq/fastq/link3', '.*(568|050|047|022|460)T.*R([12])_001\.fastq.gz')
-##SGI 201310301 samples
-link_l('/EQL2/SGI_20131031/RNASeq/fastq','/EQL2/SGI_20131031/RNASeq/fastq/link',['([0-9]{1,2}[AB]).*R([12]).fastq.gz', '.*([0-9]{3})T.*R([12]).fastq.gz'], RSQ=True)
-link('/EQL2/SGI_20131031/WXS/fastq','/EQL2/SGI_20131031/WXS/fastq/link','([0-9]{1,2}C).*R([12]).fastq.gz', normalL=['10C','11C','12C','3C','7C','9C'])
+##SGI 20131031 samples
+#link_l('/EQL2/SGI_20131031/RNASeq/fastq','/EQL2/SGI_20131031/RNASeq/fastq/link',['([0-9]{1,2}[AB]).*R([12]).fastq.gz', '.*([0-9]{3})T.*R([12]).fastq.gz'], RSQ=True)
+#link('/EQL2/SGI_20131031/WXS/fastq','/EQL2/SGI_20131031/WXS/fastq/link','([0-9]{1,2}C).*R([12]).fastq.gz', normalL=['10C','11C','12C','3C','7C','9C'])
+##SGI 20131119 samples
+#link('/EQL2/SGI_20131119/RNASeq/fastq','/EQL2/SGI_20131119/RNASeq/fastq/link','.*-([0-9]{3}).*_[ACGT]{6}_R([12]).fastq.gz',RSQ=True)
+link_l('/EQL2/SGI_20131119/WXS/fastq','/EQL2/SGI_20131119/WXS/fastq/link',['([0-9]{1,2}[AB])_[ACGT]{6}_R([12]).*.fastq.gz','.*([0-9]{3}).*_[ACGT]{6}_R([12]).fastq.gz'], normalL=['14C','8C'])
