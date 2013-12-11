3afc6a003472a91a539b7e526560c91cc79336d7
ikarus97 <ikarus97@gmail.com>
2013-12-11 19:42:05 +0900

    change in parsing sample id (due to snu samples (S**[ABC])

--- Integration/splice_fusion.sql
+++ Integration/splice_fusion.sql
@@ -19,5 +19,6 @@ CREATE TABLE splice_fusion (
 /*LOAD DATA LOCAL INFILE "/EQL1/NSL/RNASeq/alignment/splice_fusion_NSL41_p2.dat" IGNORE INTO TABLE splice_fusion;*/
 /*LOAD DATA LOCAL INFILE "/EQL1/NSL/RNASeq/alignment/splice_fusion_NSL41_p1.dat" IGNORE INTO TABLE splice_fusion;*/
 LOAD DATA LOCAL INFILE "/EQL1/NSL/RNASeq/results/fusion/splice_fusion_NSL45.dat" IGNORE INTO TABLE splice_fusion;
+LOAD DATA LOCAL INFILE "/EQL2/SGI_20131031/RNASeq/results/fusion/splice_fusion_30.dat" IGNORE INTO TABLE splice_fusion;
 
 delete from splice_fusion where gene_sym1 like 'HLA-%' and gene_sym2 like 'HLA-%';
