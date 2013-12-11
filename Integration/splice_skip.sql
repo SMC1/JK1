2ebe1c78ac74636c933c96062c3583d07d6a96ee
ikarus97 <ikarus97@gmail.com>
2013-12-11 19:40:25 +0900

    change to insert (not replace) data into 'splice_skip' table

--- Integration/splice_skip.sql
+++ Integration/splice_skip.sql
@@ -1,4 +1,4 @@
-drop table IF EXISTS splice_skip;
+/*drop table IF EXISTS splice_skip;
 CREATE TABLE splice_skip (
 	samp_id varchar(63) NOT NULL,
 	loc1 varchar(31) NOT NULL, -- hg19
@@ -12,9 +12,10 @@ CREATE TABLE splice_skip (
 	nPos tinyint unsigned NOT NULL,
 	primary key (samp_id,loc1,loc2),
 	index (samp_id,delExons)
-);
+);*/
 
 /* LOAD DATA LOCAL INFILE "/EQL1/NSL/RNASeq/alignment/splice_skip_NSL36_p5.dat" IGNORE INTO TABLE splice_skip; */
 /* LOAD DATA LOCAL INFILE "/EQL3/TCGA/GBM/RNASeq/alignment/splice_skipping_170_p5.dat" IGNORE INTO TABLE splice_skip; */
 /* LOAD DATA LOCAL INFILE "/EQL1/NSL/RNASeq/alignment/splice_skip_NSL41_p5.dat" IGNORE INTO TABLE splice_skip; */
-LOAD DATA LOCAL INFILE "/EQL1/NSL/RNASeq/results/exonSkip/splice_skip_NSL45.dat" IGNORE INTO TABLE splice_skip;
+/*LOAD DATA LOCAL INFILE "/EQL1/NSL/RNASeq/results/exonSkip/splice_skip_NSL45.dat" IGNORE INTO TABLE splice_skip; */
+LOAD DATA LOCAL INFILE "/EQL2/SGI_20131031/RNASeq/results/exonSkip/splice_skip_30.dat" IGNORE INTO TABLE splice_skip;
