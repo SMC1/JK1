419ff15a2a572760f7365e77dd7a9eb62b3e10de
ikarus97 <ikarus97@gmail.com>
2013-12-11 19:39:54 +0900

    change to insert (not replace) data into 'splice_normal' table

--- Integration/splice_normal.sql
+++ Integration/splice_normal.sql
@@ -1,4 +1,4 @@
-drop table IF EXISTS splice_normal;
+/*drop table IF EXISTS splice_normal;
 CREATE TABLE splice_normal (
 	samp_id char(12) NOT NULL,
 	loc1 varchar(31) NOT NULL, -- hg19
@@ -9,11 +9,12 @@ CREATE TABLE splice_normal (
 	index (samp_id,loc1),
 	index (samp_id,loc2),
 	index (samp_id)
-);
+);*/
 
 /* LOAD DATA LOCAL INFILE "/EQL1/NSL/RNASeq/alignment/splice_normal_NSL36.dat" INTO TABLE splice_normal; */
 /* LOAD DATA LOCAL INFILE "/EQL3/TCGA/GBM/RNASeq/alignment/splice_normal_170.dat" IGNORE INTO TABLE splice_normal; */
-LOAD DATA LOCAL INFILE "/EQL1/NSL/RNASeq/results/exonSkip_normal/splice_normal_NSL45.dat" INTO TABLE splice_normal;
+/*LOAD DATA LOCAL INFILE "/EQL1/NSL/RNASeq/results/exonSkip_normal/splice_normal_NSL45.dat" INTO TABLE splice_normal; */
+LOAD DATA LOCAL INFILE "/EQL2/SGI_20131031/RNASeq/results/exonSkip_normal/splice_normal_30.dat" INTO TABLE splice_normal;
 
 drop table if exists splice_normal_loc1;
 create table splice_normal_loc1 as select samp_id,loc1,sum(nReads) nReads_w1 from splice_normal group by samp_id,loc1;
