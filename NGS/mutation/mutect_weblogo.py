083c6dc107d508cd3b65cefbbb044dc49c7f1de1
ikarus97 <ikarus97@gmail.com>
2015-03-18 16:36:11 +0900

    Batch routine for drawing weblogo using mutect mutations

--- NGS/mutation/mutect_weblogo.py
+++ NGS/mutation/mutect_weblogo.py
@@ -3,7 +3,22 @@
 import sys, os, weblogolib
 import mybasic
 
-def mutect_weblogo(sampN, inFileN, outFileN):
+def mutect_weblogo(inDirN, outDirN):
+	sampN = os.path.basename(inDirN)
+	outFileN = '%s/%s.weblogo_C2T.input' % (outDirN, sampN)
+	pdfFileN = '%s/%s.weblogo_C2T.pdf' % (outDirN, sampN)
+
+	mutFileNL = map(lambda x: x.rstrip(), os.popen('ls %s/*mutect' % inDirN).readlines())
+	if mutFileNL == []:
+		mutFileNL = map(lambda x: x.rstrip(), os.popen('ls %s/*rerun' % inDirN).readlines())
+	
+	if len(mutFileNL)>1:
+		print 'Multiple input files!'
+		sys.exit(1)
+	
+	mutect_weblogo_sub(sampN, mutFileNL[0], outFileN, pdfFileN)
+
+def mutect_weblogo_sub(sampN, inFileN, outFileN, pdfFileN):
 	inFile = open(inFileN, 'r')
 	inFile.readline() #comment line
 	headerL = inFile.readline().rstrip().split('\t')
@@ -11,7 +26,7 @@ def mutect_weblogo(sampN, inFileN, outFileN):
 	for i in range(len(headerL)):
 		idxH[headerL[i]] = i
 
-	tmpFile = open('tmp','w')
+	outFile = open(outFileN,'w')
 	for line in inFile:
 		colL = line.rstrip().split('\t')
 		context = colL[idxH['context']]
@@ -30,11 +45,11 @@ def mutect_weblogo(sampN, inFileN, outFileN):
 			alt = mybasic.rc(alt)
 
 		if ref == 'C' and alt == 'T':## TMZ context only
-			tmpFile.write('%s\n' % context)
-	tmpFile.flush()
-	tmpFile.close()
+			outFile.write('%s\n' % context)
+	outFile.flush()
+	outFile.close()
 	
-	fin = open('tmp','r')
+	fin = open(outFileN,'r')
 	seqs = weblogolib.read_seq_data(fin)
 	data = weblogolib.LogoData.from_seqs(seqs)
 	options = weblogolib.LogoOptions()
@@ -42,13 +57,13 @@ def mutect_weblogo(sampN, inFileN, outFileN):
 	options.first_index = -3
 	options.logo_title = sampN
 	format = weblogolib.LogoFormat(data, options)
-	fout = open(outFileN, 'w')
+	fout = open(pdfFileN, 'w')
 	weblogolib.pdf_formatter(data, format, fout)
-	os.system('mv tmp  %s.weblogo.input' % sampN)
 
 if __name__ == '__main__':
-	mutect_weblogo('S302','/EQL3/pipeline/somatic_mutect/S302_T_SS.mutect','S302.weblogo.pdf')
-	mutect_weblogo('S171','/EQL3/pipeline/somatic_mutect/S171_T_SS.mutect','S171.weblogo.pdf')
-	mutect_weblogo('S585','/EQL3/pipeline/somatic_mutect/S585_T_SS.mutect','S585.weblogo.pdf')
-
-#	contig	position	context	ref_allele	alt_allele	tumor_name	normal_name	score	dbsnp_sitecovered	power	tumor_power	normal_power	total_pairs	improper_pairs	map_Q0_reads	t_lod_fstar	tumor_f	contaminant_fraction	contaminant_lod	t_ref_count	t_alt_count	t_ref_sum	t_alt_sum	t_ref_max_mapq	t_alt_max_mapq	t_ins_count	t_del_count	normal_best_gt	init_n_lod	n_ref_count	n_alt_count	n_ref_sum	n_alt_sum	judgement
+#	mutect_weblogo('S302','/EQL3/pipeline/somatic_mutect/S302_T_SS.mutect','S302.weblogo.pdf')
+#	mutect_weblogo('S171','/EQL3/pipeline/somatic_mutect/S171_T_SS.mutect','S171.weblogo.pdf')
+#	mutect_weblogo('S585','/EQL3/pipeline/somatic_mutect/S585_T_SS.mutect','S585.weblogo.pdf')
+	for id in ['IRCR_GBM10_038_T_SS','IRCR_GBM12_199_T_SS','IRCR_GBM14_412_T_SS','IRCR_GBM_363_TM_SS','IRCR_GBM_363_TD_SS','IRCR_GBM14_366_T_SS','IRCR_GBM14_476_T03_SS','S171_T_SS','S302_T_SS']:
+		print id
+		mutect_weblogo('/EQL3/pipeline/somatic_mutation/%s' % id, '/EQL3/pipeline/somatic_mutation/%s' % id)
